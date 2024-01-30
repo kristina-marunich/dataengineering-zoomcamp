24e51fcd14178bcbbd88751546c138a17fb5831a
kristina-marunich <kristinamaximyuk@gmail.com>
2024-01-30 21:50:42 +0100

    third commit

---
+++ homework1_terraform/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.13.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials)
+  project     = "dataeng-376620"
+  region      = "europe-west3"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "dataeng-376620-terra-bucket"
+  location      = "EU"
+  force_destroy = true
+
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+resource "google_bigquery_dataset" "demo-dataset" {
+  dataset_id = var.bq_dataset_name
+  location = var.location
+}
